84569845f8c12adcb1c5485d0aa5db47cfbf8ee4
George Georgopoulos <georgopoulos.georgios@gmail.com>
2019-05-20 16:22:12 +0300

    Added copyright file header.

--- editor/src/main/java/com/nepheletech/jred/editor/api/servlet/CredentialsServlet.java
+++ editor/src/main/java/com/nepheletech/jred/editor/api/servlet/CredentialsServlet.java
@@ -1,18 +1,20 @@
 /*
- *     This file is part of J-RED project.
+ * Copyright NepheleTech, http://www.nephelerech.com
  *
- *   J-RED is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ * This file is part of J-RED project.
  *
- *   J-RED is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ * J-RED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with J-RED.  If not, see <https://www.gnu.org/licenses/>.
+ * J-RED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with J-RED.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.nepheletech.jred.editor.api.servlet;
 
